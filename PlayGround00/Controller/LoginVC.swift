04bac4ca8891310bdc806f3446f130575e0be1f8
Shaun Tucker <tucke@Shauns-MacBook-Air.local>
2018-06-27 13:43:46 -0400

    Base 2 TabBars

---
+++ PlayGround00/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  PlayGround00
+//
+//  Created by Shaun Tucker on 6/27/18.
+//  Copyright © 2018 Shaun Tucker. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+
+}
